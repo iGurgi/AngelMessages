95022bc7b4eefa9e6446e6ec3404180251b2b6cb
iGurgi <igurgi@gmail.com>
2026-05-12 12:07:00 -0600

    Simplify schedule provider

--- lib/providers/schedule_providers.dart
+++ lib/providers/schedule_providers.dart
@@ -3,7 +3,10 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'schedule_providers.g.dart';
 
 @riverpod
-Future<bool> notificationsEnabled(NotificationsEnabledRef ref) async {
-  // TODO: Implement checking notification settings
-  return false;
+class NotificationsEnabled extends _$NotificationsEnabled {
+  @override
+  bool build() {
+    // TODO: Check actual notification settings
+    return false;
+  }
 }
