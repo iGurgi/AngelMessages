66cd448baae14475b9484867121a1b2e84d5d001
iGurgi <igurgi@gmail.com>
2026-05-12 12:09:15 -0600

    Create minimal working app for CI testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:angel_messages/router/app_router.dart';
-import 'package:angel_messages/theme/app_theme.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(
-    const ProviderScope(
-      child: AngelMessagesApp(),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
-class AngelMessagesApp extends StatelessWidget {
-  const AngelMessagesApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       title: 'Angel Messages',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.dark,
-      routerConfig: appRouter,
-      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Angel Messages'),
+        ),
+      ),
     );
   }
 }
