16bdf39a845a06b83dfea21cd158a8f8624506da
iGurgi <igurgi@gmail.com>
2026-05-12 12:33:09 -0600

    Add placeholder message.g.dart for initial pub get

---
+++ lib/features/messages/data/models/message.g.dart
@@ -0,0 +1,6 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// This is a placeholder file to allow pub get to succeed
+// Run: dart run build_runner build --delete-conflicting-outputs
+// to generate the actual implementation
+
+part of 'message.dart';
