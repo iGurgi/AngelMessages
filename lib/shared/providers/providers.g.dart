9c69d0d48d1bd66cad34b348f0a061b4d67c4f05
iGurgi <igurgi@gmail.com>
2026-05-12 12:35:53 -0600

    Remove providers.g.dart placeholder - will be generated by build_runner

--- lib/shared/providers/providers.g.dart
+++
@@ -1,6 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// This is a placeholder file to allow pub get to succeed
-// Run: dart run build_runner build --delete-conflicting-outputs
-// to generate the actual implementation
-
-part of 'providers.dart';
