be7d4506e0330d4c11b4030d6c4b5978388cfb1a
iGurgi <igurgi@gmail.com>
2026-05-12 12:23:43 -0600

    Add main.dart with complete app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:app_links/app_links.dart';
+import 'package:angel_messages/core/router/app_router.dart';
+import 'package:angel_messages/core/theme/app_theme.dart';
+import 'package:angel_messages/features/background/services/background_sync_service.dart';
+import 'package:angel_messages/features/notifications/services/notification_scheduler.dart';
+import 'package:angel_messages/features/permissions/services/permission_service.dart';
+import 'package:angel_messages/features/settings/providers/schedule_notifier.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Set preferred orientations
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  // Initialize background sync
+  await BackgroundSyncService.initialize();
+  await BackgroundSyncService.registerDailySync();
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  late AppLinks _appLinks;
+  NotificationScheduler? _notificationScheduler;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeApp();
+    _setupDeepLinking();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Future<void> _initializeApp() async {
+    // Initialize notification scheduler
+    _notificationScheduler = ref.read(notificationSchedulerProvider);
+    await _notificationScheduler!.initialize();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    // Request notification permission
+    if (mounted) {
+      final granted = await PermissionService.requestNotificationPermission(context);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+      if (granted) {
+        // Schedule notifications based on saved preference
+        final schedule = await ref.read(scheduleNotifierProvider.future);
+        await _notificationScheduler!.scheduleNotifications(schedule);
+      } else if (mounted) {
+        // Show persistent snackbar if denied
+        PermissionService.showPermissionDeniedSnackBar(context);
+      }
+    }
 
-  final String title;
+    // Set the scheduler in the notifier
+    ref.read(scheduleNotifierProvider.notifier).setScheduler(_notificationScheduler!);
+  }
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void _setupDeepLinking() {
+    _appLinks = AppLinks();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    // Handle initial link if app was cold-started from a notification
+    _appLinks.getInitialLink().then((uri) {
+      if (uri != null) {
+        _handleDeepLink(uri);
+      }
+    });
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    // Handle links while app is running
+    _appLinks.uriLinkStream.listen((uri) {
+      _handleDeepLink(uri);
     });
   }
 
+  void _handleDeepLink(Uri uri) {
+    // Handle angelmessages://message/:id
+    if (uri.scheme == 'angelmessages' && uri.host == 'message') {
+      final id = uri.pathSegments.isNotEmpty ? uri.pathSegments.first : null;
+      if (id != null) {
+        appRouter.push('/message/$id');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return MaterialApp.router(
+      title: 'Angel Messages',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.dark, // Dark mode first
+      routerConfig: appRouter,
     );
   }
 }
