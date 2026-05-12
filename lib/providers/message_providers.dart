a303956bfb9b010c105dfd79e7755a60a2939354
iGurgi <igurgi@gmail.com>
2026-05-12 11:50:02 -0600

    Simplify message providers to basic structure

--- lib/providers/message_providers.dart
+++ lib/providers/message_providers.dart
@@ -1,45 +1,9 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:angel_messages/data/message_repository.dart';
-import 'package:angel_messages/models/message.dart';
-import 'package:angel_messages/providers/repository_providers.dart';
 
 part 'message_providers.g.dart';
 
 @riverpod
-class AllMessages extends _$AllMessages {
-  @override
-  Future<List<Message>> build() async {
-    final repository = ref.watch(messageRepositoryProvider);
-    return repository.getAllMessages();
-  }
-
-  Future<void> refresh() async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() async {
-      final repository = ref.read(messageRepositoryProvider);
-      await repository.syncFromRemote();
-      return repository.getAllMessages();
-    });
-  }
-}
-
-@riverpod
-class MessageDetail extends _$MessageDetail {
-  @override
-  Future<Message?> build(String messageId) async {
-    final repository = ref.watch(messageRepositoryProvider);
-    return repository.getMessageById(messageId);
-  }
-
-  Future<void> markAsViewed(String messageId) async {
-    final repository = ref.read(messageRepositoryProvider);
-    await repository.markAsViewed(messageId);
-    ref.invalidate(allMessagesProvider);
-  }
-}
-
-@riverpod
-Future<Message?> nextUnviewedMessage(NextUnviewedMessageRef ref) async {
-  final repository = ref.watch(messageRepositoryProvider);
-  return repository.getNextUnviewedMessage();
+Future<List<String>> messages(MessagesRef ref) async {
+  // TODO: Implement message fetching
+  return [];
 }
