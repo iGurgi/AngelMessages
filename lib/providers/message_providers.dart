20510072edb059720a6f6b75e27ff55194e6d12d
iGurgi <igurgi@gmail.com>
2026-05-12 12:06:29 -0600

    Simplify message provider

--- lib/providers/message_providers.dart
+++ lib/providers/message_providers.dart
@@ -3,7 +3,10 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'message_providers.g.dart';
 
 @riverpod
-Future<List<String>> messages(MessagesRef ref) async {
-  // TODO: Implement message fetching
-  return [];
+class Messages extends _$Messages {
+  @override
+  Future<List<String>> build() async {
+    // TODO: Implement message fetching
+    return [];
+  }
 }
