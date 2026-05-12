3d1ccb820e3441464acdfe489caf2cd019b8a839
iGurgi <igurgi@gmail.com>
2026-05-12 12:05:39 -0600

    Fix schedule provider to return Future

--- lib/providers/schedule_providers.dart
+++ lib/providers/schedule_providers.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'schedule_providers.g.dart';
 
 @riverpod
-bool notificationsEnabled(NotificationsEnabledRef ref) {
+Future<bool> notificationsEnabled(NotificationsEnabledRef ref) async {
+  // TODO: Implement checking notification settings
   return false;
 }
