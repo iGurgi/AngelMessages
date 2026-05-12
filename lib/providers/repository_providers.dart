8a2dbab6e6631de2b3af5382736f1d969873032f
iGurgi <igurgi@gmail.com>
2026-05-12 12:05:48 -0600

    Fix repository provider to return Future

--- lib/providers/repository_providers.dart
+++ lib/providers/repository_providers.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'repository_providers.g.dart';
 
 @riverpod
-String appVersion(AppVersionRef ref) {
+Future<String> appVersion(AppVersionRef ref) async {
+  // TODO: Get version from package info
   return '1.0.0';
 }
