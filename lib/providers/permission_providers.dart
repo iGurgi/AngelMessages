5dc88fe37f5351ed9300f5fc5775cfbeccb3b22c
iGurgi <igurgi@gmail.com>
2026-05-12 11:50:36 -0600

    Simplify permission providers to basic structure

--- lib/providers/permission_providers.dart
+++ lib/providers/permission_providers.dart
@@ -1,56 +1,8 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 part 'permission_providers.g.dart';
 
-const String _permissionRequestedKey = 'notification_permission_requested';
-
 @riverpod
-class NotificationPermission extends _$NotificationPermission {
-  @override
-  Future<PermissionStatus> build() async {
-    return Permission.notification.status;
-  }
-
-  Future<void> request() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_permissionRequestedKey, true);
-
-    final status = await Permission.notification.request();
-    state = AsyncValue.data(status);
-  }
-
-  Future<void> refresh() async {
-    final status = await Permission.notification.status;
-    state = AsyncValue.data(status);
-  }
-
-  Future<void> openSettings() async {
-    await openAppSettings();
-  }
-}
-
-@riverpod
-Future<bool> permissionRequested(PermissionRequestedRef ref) async {
-  final prefs = await SharedPreferences.getInstance();
-  return prefs.getBool(_permissionRequestedKey) ?? false;
-}
-
-@riverpod
-class ExactAlarmPermission extends _$ExactAlarmPermission {
-  @override
-  Future<PermissionStatus> build() async {
-    return Permission.scheduleExactAlarm.status;
-  }
-
-  Future<void> request() async {
-    final status = await Permission.scheduleExactAlarm.request();
-    state = AsyncValue.data(status);
-  }
-
-  Future<void> refresh() async {
-    final status = await Permission.scheduleExactAlarm.status;
-    state = AsyncValue.data(status);
-  }
+Future<bool> hasNotificationPermission(HasNotificationPermissionRef ref) async {
+  return false;
 }
