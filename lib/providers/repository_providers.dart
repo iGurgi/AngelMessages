277f1a2705fdda29d3fc0ef9e45ba95e0fdbba09
iGurgi <igurgi@gmail.com>
2026-05-12 12:06:51 -0600

    Simplify repository provider

--- lib/providers/repository_providers.dart
+++ lib/providers/repository_providers.dart
@@ -3,7 +3,10 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'repository_providers.g.dart';
 
 @riverpod
-Future<String> appVersion(AppVersionRef ref) async {
-  // TODO: Get version from package info
-  return '1.0.0';
+class AppVersion extends _$AppVersion {
+  @override
+  String build() {
+    // TODO: Get version from package info
+    return '1.0.0';
+  }
 }
