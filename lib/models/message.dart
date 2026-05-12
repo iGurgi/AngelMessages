68b69ac44cec42695363840d75c0b1d063ccc772
iGurgi <igurgi@gmail.com>
2026-05-12 11:35:28 -0600

    Update Message model to remove Isar annotations

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,11 +1,9 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:isar/isar.dart';
 
 part 'message.freezed.dart';
 part 'message.g.dart';
 
 @freezed
-@Collection(ignore: {'copyWith'})
 class Message with _$Message {
   const Message._();
 
@@ -20,27 +18,4 @@ class Message with _$Message {
 
   factory Message.fromJson(Map<String, dynamic> json) =>
       _$MessageFromJson(json);
-
-  @override
-  Id get isarId => fastHash(id);
-
-  @Index()
-  bool get isViewed => viewed;
-
-  @Index()
-  DateTime get isarCreatedAt => createdAt;
-}
-
-/// Fast string hash for Isar ID generation
-int fastHash(String string) {
-  var hash = 0xcbf29ce484222325;
-  var i = 0;
-  while (i < string.length) {
-    final codeUnit = string.codeUnitAt(i++);
-    hash ^= codeUnit >> 8;
-    hash *= 0x100000001b3;
-    hash ^= codeUnit & 0xFF;
-    hash *= 0x100000001b3;
-  }
-  return hash;
 }
